17d5b2f71dfe9819d1148200eb797dfdff0b6769
TK <ottertut@gmail.com>
2023-02-22 18:57:28 +0200

    1

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import Carousel from '../../6-module/3-task/index.js';
-import slides from '../../6-module/3-task/slides.js';
+import Carousel from './carousel.js';
+import slides from './slides.js';
 
-import RibbonMenu from '../../7-module/1-task/index.js';
-import categories from '../../7-module/1-task/categories.js';
+import RibbonMenu from './ribbonMenu.js';
+import categories from './categories.js';
 
-import StepSlider from '../../7-module/4-task/index.js';
-import ProductsGrid from '../../8-module/2-task/index.js';
+import StepSlider from './stopSlider.js';
+import ProductsGrid from './productGrid.js';
 
-import CartIcon from '../../8-module/1-task/index.js';
-import Cart from '../../8-module/4-task/index.js';
+import CartIcon from './cartIcon.js';
+import Cart from './cart.js';
 
 export default class Main {
 
